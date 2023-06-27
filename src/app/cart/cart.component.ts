6d795ad586a559271411b03810030ac9cd65f55c
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-06-27 16:42:21 -0700

    created cart component

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,95 @@
+import {Component, OnInit} from '@angular/core';
+import {CertificateService} from '../shared/services/certificate.service';
+import {forkJoin} from "rxjs";
+import {Certificate} from '../models/ICertificate';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+  cart: { certificate: Certificate, quantity: number }[] = [];
+
+  constructor(private certificateService: CertificateService) {
+  }
+
+  ngOnInit(): void {
+    const cartData = localStorage.getItem('cart');
+    const cartIds = JSON.parse(cartData || '[]');
+
+    cartIds.forEach((item: any) => {
+      this.certificateService.getCertificate(item.id).subscribe(
+        certificate => {
+          this.cart.push({
+            certificate: certificate,
+            quantity: item.quantity
+          });
+        },
+        error => {
+          console.error('Error loading certificate', error);
+        }
+      );
+    });
+  }
+
+  removeFromCart(
+    item: {
+      certificate: Certificate;
+      quantity: number
+    }
+  ): void {
+    const cartData = localStorage.getItem('cart');
+    const cart: { id: number, quantity: number }[] = cartData ? JSON.parse(cartData) : [];
+    const index = cart.findIndex((cartItem: { id: number, quantity: number }) => cartItem.id === item.certificate.id);
+    if (index !== -1) {
+      cart.splice(index, 1);
+      localStorage.setItem('cart', JSON.stringify(cart));
+      this.updateCart();
+    }
+  }
+
+  getTotalPrice(): number {
+    return Math.round(
+      this.cart.reduce(
+        (
+          total: number, item: {
+            certificate: Certificate;
+            quantity: number
+          }
+        ) => total + item.certificate.price * item.quantity, 0
+      )
+    );
+  }
+
+  changeQuantity(item: {certificate: Certificate, quantity: number}): void {
+    const cartData = localStorage.getItem('cart');
+    const cart: {id: number, quantity: number}[] = cartData ? JSON.parse(cartData) : [];
+    const cartItem = cart.find(cartItem => cartItem.id === item.certificate.id);
+    if (cartItem) {
+      cartItem.quantity = item.quantity;
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+  }
+
+
+  updateCart(): void {
+    const cartData = localStorage.getItem('cart');
+    const cart: {
+      id: number, quantity: number
+    }[] = cartData ? JSON.parse(cartData) : [];
+    const requests = cart.map(itemId => this.certificateService.getCertificate(itemId.id));
+    forkJoin(requests).subscribe(
+      (responses: Certificate[]) => {
+        this.cart = responses.map((item, index) => ({
+            certificate: item,
+            quantity: cart.filter(itemId => itemId.id === item.id).length
+          })
+        );
+      }
+    );
+  }
+
+  addToFavorites(certificate: Certificate): void {
+  }
+}
