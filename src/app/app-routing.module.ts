0af1c541f8177305e6baba442f88dafa7c220bc4
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-06-27 15:14:49 -0700

    added module imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {CertificateComponent} from "./certificate/certificate.component";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {CartComponent} from "./cart/cart.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  { path: 'certificates/:id', component: CertificateComponent },
-  { path: 'error/:errorCode/:errorMessage', component: ErrorPageComponent },
-  { path: '**', redirectTo: '/error/404/Page not found' }
+  {path: 'cart', component: CartComponent},
+  {path: 'certificates/:id', component: CertificateComponent},
+  {path: 'error/:errorCode/:errorMessage', component: ErrorPageComponent},
+  {path: '**', redirectTo: '/error/404/Page not found'}
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
-import { CartComponent } from './cart/cart.component';
 
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { CertificatesComponent } from './certificates/certificates.component';
 import { CertificateComponent } from './certificate/certificate.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+
 
 @NgModule({
   declarations: [
@@ -20,16 +26,21 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     CartComponent,
     CertificatesComponent,
     CertificateComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatSnackBarModule,
     AuthModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
