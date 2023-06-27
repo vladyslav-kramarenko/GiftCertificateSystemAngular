65b5cdf831461fc44f0d686b6b445b7de48519ee
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-06-26 18:56:47 -0700

    created certificate page

---
+++ src/app/certificate/certificate.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CertificateService} from "./certificate.service";
+import {Router} from '@angular/router';
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'app-certificate',
+  templateUrl: './certificate.component.html',
+  styleUrls: ['./certificate.component.scss']
+})
+export class CertificateComponent implements OnInit {
+  certificate: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private certificateService: CertificateService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      const id = +idParam;
+      this.certificateService.getCertificate(id).subscribe(
+        data => this.certificate = data,
+        (error: HttpErrorResponse) => {
+          // handle error when certificate not found
+          let errorMessage;
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+          this.router.navigateByUrl(`/error/${error.status}/${errorMessage}`);
+        }
+      );
+    } else {
+      // redirect to 404 error page when 'id' parameter is not available
+      this.router.navigateByUrl('/error/400/Bad Request');
+    }
+  }
+}
