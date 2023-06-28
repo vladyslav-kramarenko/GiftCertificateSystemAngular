b57e7aba6d5285c28317b6720acbf16e4e0c336c
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-06-27 21:25:34 -0700

    added favorite button component import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,23 +4,26 @@ import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {RouterLink} from "@angular/router";
 import {AppScrollButtonsComponent} from './app-scroll-buttons/app-scroll-buttons.component';
+import { FavoriteButtonComponent } from './favorite-button/favorite-button.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    AppScrollButtonsComponent
+    AppScrollButtonsComponent,
+    FavoriteButtonComponent
   ],
   imports: [
     CommonModule,
     RouterLink
   ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    AppScrollButtonsComponent
-  ]
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        AppScrollButtonsComponent,
+        FavoriteButtonComponent
+    ]
 })
 export class SharedModule {
 }
