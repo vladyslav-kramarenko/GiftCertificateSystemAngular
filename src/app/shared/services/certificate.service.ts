0a3db2bf5f3439e0180938d89ec8a1c58780fe03
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-06-28 17:13:32 -0700

    added get certificates method

--- src/app/shared/services/certificate.service.ts
+++ src/app/shared/services/certificate.service.ts
@@ -1,7 +1,14 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {environment} from "../../../environments/environment";
+import {map} from "rxjs/operators";
+
+interface CertificateResponse {
+  _embedded: {
+    singleGiftCertificateDTOList: any[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +19,21 @@ export class CertificateService {
   }
 
   getCertificate(id: number): Observable<any> {
+    console.log(`Getting certificate with id: ${id}`);
     return this.http.get<any>(`${environment.API_URL}/certificates/${id}`);
+  }
+
+  getCertificates(page: number, size: number): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('page', page.toString());
+    params = params.append('size', size.toString());
+
+    return this.http.get<CertificateResponse>(`${environment.API_URL}/certificates`, {params: params}).pipe(
+      map(response => {
+        // map the '_embedded.singleGiftCertificateDTOList' from the response to an array of certificates
+        // return response['_embedded']['singleGiftCertificateDTOList'];
+        return response._embedded.singleGiftCertificateDTOList;
+      })
+    );
   }
 }
