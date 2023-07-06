dbafb6c750c6db6fb3c4c78fab63f5014cbdc295
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-06 15:53:12 -0700

    added mat dialog, confirm dialog modules import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,28 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from "@angular/material/table";
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
 
 import {AuthModule} from './auth/auth.module';
 import {SharedModule} from './shared/shared.module';
 import {AppRoutingModule} from './app-routing.module';
 
-import {CertificatesComponent} from './certificates/certificates.component';
+import {CertificateEditComponent} from './certificate-edit/certificate-edit.component';
 import {CertificateInfoComponent} from './certificate-info/certificate.component';
+import {CertificatesComponent} from './certificates/certificates.component';
+import {UserOrdersComponent} from './user-orders/user-orders.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
-import {AppComponent} from './app.component';
-import {CartComponent} from './cart/cart.component';
 import {FavoritesComponent} from './favorites/favorites.component';
 import {CheckoutComponent} from './checkout/checkout.component';
-import {CertificateEditComponent} from './certificate-edit/certificate-edit.component';
-import {UserOrdersComponent} from './user-orders/user-orders.component';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatTableModule} from "@angular/material/table";
+import {CartComponent} from './cart/cart.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import {MatTableModule} from "@angular/material/table";
     MatCardModule,
     MatIconModule,
     MatTableModule,
+    MatDialogModule,
     MatProgressSpinnerModule,
   ],
   providers: [],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {RouterModule} from "@angular/router";
 import {AppScrollButtonsComponent} from './app-scroll-buttons/app-scroll-buttons.component';
-import { FavoriteButtonComponent } from './favorite-button/favorite-button.component';
+import {FavoriteButtonComponent} from './favorite-button/favorite-button.component';
+import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
 
 
 @NgModule({
@@ -13,19 +15,22 @@ import { FavoriteButtonComponent } from './favorite-button/favorite-button.compo
     HeaderComponent,
     FooterComponent,
     AppScrollButtonsComponent,
-    FavoriteButtonComponent
+    FavoriteButtonComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
+    MatDialogModule,
   ],
-    exports: [
-        HeaderComponent,
-        FooterComponent,
-        AppScrollButtonsComponent,
-        FavoriteButtonComponent
-    ]
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    AppScrollButtonsComponent,
+    FavoriteButtonComponent,
+    MatDialogModule,
+  ]
 })
 export class SharedModule {
 }
