6b475587d81dc8dfbfe72a2dd5fddf2f6ef7710c
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-10 13:47:25 -0700

    added error handling and routing to login on 401 error

--- src/app/shared/services/image.service.ts
+++ src/app/shared/services/image.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
 import {environment} from "../../../environments/environment";
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,10 @@ export class ImageService {
 
   private BASE_URL = `${environment.API_URL}/image/`;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) {
   }
 
   uploadImage(file: File): Observable<any> {
@@ -19,13 +23,20 @@ export class ImageService {
     const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
     const formData: FormData = new FormData();
     formData.append('file', file, file.name);
-    return this.http.post(this.BASE_URL, formData, {responseType: 'text',headers});
+    return this.http.post(this.BASE_URL, formData, {responseType: 'text', headers})
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   getImage(path: string): Observable<Blob> {
-    console.log("getImage");
-    const token = localStorage.getItem('authToken');
-    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
-    return this.http.get(this.BASE_URL + path, {responseType: 'blob', headers});
+    return this.http.get(this.BASE_URL + path, {responseType: 'blob'});
+  }
+
+  private handleError = (error: HttpErrorResponse) => {
+    console.log("error.status: " + error.status);
+    console.log(error.message);
+    if (error.status === 401) this.authService.updateStatusToNotAuthorized();
+    return throwError(error);
   }
 }
