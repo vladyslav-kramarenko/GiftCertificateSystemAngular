c4b6e62aa26bcad274a2d06f291c0257c7045cb4
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-13 16:16:50 -0700

    cleaning

--- src/app/shared/services/image.service.ts
+++ src/app/shared/services/image.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {environment} from "../../../environments/environment";
-import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -12,31 +11,17 @@ export class ImageService {
   private BASE_URL = `${environment.API_URL}/image/`;
 
   constructor(
-    private http: HttpClient,
-    private authService: AuthService,
+    private http: HttpClient
   ) {
   }
 
   uploadImage(file: File): Observable<any> {
-    console.log("uploadImage");
-    const token = localStorage.getItem('authToken');
-    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
     const formData: FormData = new FormData();
     formData.append('file', file, file.name);
-    return this.http.post(this.BASE_URL, formData, {responseType: 'text', headers})
-      .pipe(
-        catchError(this.handleError)
-      );
+    return this.http.post(this.BASE_URL, formData, {responseType: 'text'});
   }
 
   getImage(path: string): Observable<Blob> {
     return this.http.get(this.BASE_URL + path, {responseType: 'blob'});
-  }
-
-  private handleError = (error: HttpErrorResponse) => {
-    console.log("error.status: " + error.status);
-    console.log(error.message);
-    if (error.status === 401) this.authService.updateStatusToNotAuthorized();
-    return throwError(error);
   }
 }
